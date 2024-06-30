09927f16393cd3c9727391bf63a6256f0b5d6380
abhishekshankr <abhishekshankar241@gmail.com>
2024-06-30 13:31:34 -0700

    Rolling back changes

--- api/figmaData.js
+++ api/figmaData.js
@@ -13,11 +13,11 @@ export default async function(req, res) {
               // Add headers if required by the Figma API
               headers: {
                 "Accept": "application/json",
-                //"Accept-Language": "en-US,en;q=0.9",
+                "Accept-Language": "en-US,en;q=0.9",
                 "Content-Type": "application/json",
-                //"tsid": "IcCc7EQuUAOq2aR8",
-                //"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
-                //"X-Csrf-Bypass": "yes"
+                "tsid": "IcCc7EQuUAOq2aR8",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
+                "X-Csrf-Bypass": "yes"
               }
           });
           const data = await figmaResponse.json();
